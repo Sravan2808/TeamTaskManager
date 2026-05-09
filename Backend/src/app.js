860edccadff14bd0ee79df0212dcb9049a0f104f
Sravan Surya <vsravansurya@gmail.com>
2026-05-09 14:07:36 +0530

    Add favicon, icons, and index.html for initial setup
    - Added favicon.svg for the website's favicon.
    - Created icons.svg containing various SVG icons for use in the application.
    - Introduced index.html as the main entry point for the web application, including meta tags and linking to assets.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,25 +6,30 @@ import authRoutes from "./routes/auth.routes.js";
 import projectRoutes from "./routes/projects.route.js";
 import taskRoutes from "./routes/task.route.js";
 import userRoutes from "./routes/user.route.js";
+import path from "path";
 
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("./public"));
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
-app.get("/", (req, res) => {
-  res.send("Backend Running Successfully");
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
 });
 
 export default app;
